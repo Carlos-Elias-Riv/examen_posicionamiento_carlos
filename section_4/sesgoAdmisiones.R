887c8b736a8d20c4d9a71e4434a19985ce06fff5
Carlos Rivera <carlos.rivera311202@gmail.com>
2023-09-28 16:25:35 -0600

    haciendo modificaciones a mi archivo de R

--- section_4/sesgoAdmisiones.R
+++ section_4/sesgoAdmisiones.R
@@ -2,26 +2,39 @@ library(ggplot2)
 
 df <- read.csv("datos_admisiones.csv", header= T)
 
+
+# sesgos en el numero de aplicaciones
 theme_set(theme_light())
 g <- ggplot(df, aes(x = Sexo, fill=Sexo)) + 
   facet_wrap(~Facultad.a.la.que.aplicó) + 
-  labs(title= "Mostrar sesgos sin discriminar a los que fueron admitidos los que no")+
+  labs(title= "Sesgos en el numero de aplicacion por facultad",
+       ylabel= "# aplicaciones x facu")+
   geom_bar(stat = "count")
 g
 
-
+# sesgos en la aceptacion de aplicaciones
 df_admitidos <- df[df$Fue.admitido == "S", ]
 
 g <- ggplot(df_admitidos, aes(x = Sexo, fill=Sexo)) + 
   facet_wrap(~Facultad.a.la.que.aplicó) + 
-  labs(title= "Sesgos en la aceptacion de la admision")+
+  labs(title= "Sesgos en la aceptacion de la aplicacion",
+       ylabel= " aceptados x facu")+
   geom_bar(stat = "count")
 g
 
+# sesgos en el rechazo de aplicaciones
 df_rechazados <- df[df$Fue.admitido == "N", ]
 
 g <- ggplot(df_rechazados, aes(x = Sexo, fill=Sexo)) + 
   facet_wrap(~Facultad.a.la.que.aplicó) + 
-  labs(title= "Sesgos en el rechazo de la admision")+
-  geom_bar(stat = "count")
+  geom_bar(stat = "count")+
+  labs(title= "Sesgos en el rechazo de la aplicacion")
+
 g
+
+
+
+
+
+
+
