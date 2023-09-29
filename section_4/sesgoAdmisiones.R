2e9898819e3aa2a884736ed64b628bfd67b769c9
Carlos Rivera <carlos.rivera311202@gmail.com>
2023-09-29 09:19:09 -0600

    agregando las gráficas finales que muestran donde están los sesgos

--- section_4/sesgoAdmisiones.R
+++ section_4/sesgoAdmisiones.R
@@ -1,14 +1,19 @@
 library(ggplot2)
 
 df <- read.csv("datos_admisiones.csv", header= T)
-
+# cuantos aplican a la escuela
+theme_set(theme_light())
+g <- ggplot(df, aes(x = Sexo, fill=Sexo)) +
+  labs(title= "Cuantos aplican a la escuela")+
+  geom_bar(stat = "count")
+g
 
 # sesgos en el numero de aplicaciones
-theme_set(theme_light())
+
 g <- ggplot(df, aes(x = Sexo, fill=Sexo)) + 
   facet_wrap(~Facultad.a.la.que.aplicó) + 
-  labs(title= "Sesgos en el numero de aplicacion por facultad",
-       ylabel= "# aplicaciones x facu")+
+  labs(title= "Cuantos aplican por facultad",
+       subtitle = "Por facultad")+
   geom_bar(stat = "count")
 g
 
@@ -17,8 +22,8 @@ df_admitidos <- df[df$Fue.admitido == "S", ]
 
 g <- ggplot(df_admitidos, aes(x = Sexo, fill=Sexo)) + 
   facet_wrap(~Facultad.a.la.que.aplicó) + 
-  labs(title= "Sesgos en la aceptacion de la aplicacion",
-       ylabel= " aceptados x facu")+
+  labs(title= "Cuantos fueron admitidos de todos los que aplicaron",
+       subtitle = "Por facultad")+
   geom_bar(stat = "count")
 g
 
@@ -28,12 +33,26 @@ df_rechazados <- df[df$Fue.admitido == "N", ]
 g <- ggplot(df_rechazados, aes(x = Sexo, fill=Sexo)) + 
   facet_wrap(~Facultad.a.la.que.aplicó) + 
   geom_bar(stat = "count")+
-  labs(title= "Sesgos en el rechazo de la aplicacion")
+  labs(title= "Cuantos fueron rechazados de todos los que aplicaron",
+       subtitle= "Por facultad")
 
 g
 
+palet_colors <- c("#77DD77", "#FF6961")
+# falta contestar, de todos los que aplicaron (por genero) que porcentaje fue admitido
+g <- ggplot(df, aes(x=Sexo)) +
+  scale_fill_manual(name= "Admitido", labels = c("Sí", "No"), values = palet_colors)+
+  facet_wrap(~Facultad.a.la.que.aplicó) +
+  geom_bar(aes(fill=Fue.admitido)) +
+  labs(title = "De todos los aplicantes por género que proporción fue admitida",
+       subtitle= "Por facultad")
+g
 
-
+g <- ggplot(df, aes(x=Sexo)) +
+  scale_fill_manual(name= "Admitido", labels = c("Sí", "No"), values = palet_colors)+
+  geom_bar(aes(fill=Fue.admitido)) +
+  labs(title = "De todos los aplicantes por género que proporción fue admitida")
+g
 
 
 
