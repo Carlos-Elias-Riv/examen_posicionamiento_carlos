7e3324c374d392ffbf0153ecc70fbb4e069b02b2
Carlos Rivera <carlos.rivera311202@gmail.com>
2023-09-29 21:19:50 -0600

    los casi últimos ajustes de la sección 3

--- section_3/main.py
+++ section_3/main.py
@@ -17,44 +17,26 @@ class Stack():
         self.head = elem
         self.stack.append(elem)
         self.size += 1
-        self.operaciones.append(1)
+        self.operaciones.append("i")
 
     def isEmpty(self):
         return self.size == 0
 
     def pop(self):
         if not self.isEmpty():
-            self.operaciones.append(0)
+            self.operaciones.append("o")
             value = self.stack.pop(-1)
             self.size -= 1
             if not self.isEmpty():
                 self.head = self.stack[-1]
+            else:
+                self.head = ""
             return value
 
         else: 
             raise Exception("Pila vacia")
 
     
-
-pila = Stack()
-
-cad = "carlos"
-
-for c in cad: 
-    pila.push(c)
-newcad = ""
-
-while not pila.isEmpty():
-    print(f'El elemento hasta arriba de la pila es: {pila.head}')
-    newcad += pila.pop()
-
-print(newcad)
-
-
-
-
-
-
 
 # ideas: 
 # podré hacer que el stack tenga memoria de las operaciones que se han hecho con ella? valdrá la pena?
@@ -75,34 +57,85 @@ print(newcad)
 
 # word1 –transformar--> word2 desde la posicion=pos
 # neword es la palabra que se debe volver word2
-def allposibleseqRec(word1, word2, pos, neword, pila, resp):
+def allposibleseqRec(word1, word2, pos1, pos2, neword, pila, resp):
     # caso base fracaso
-    if pos >= len(word1):
-        return 
+    # puede ser tambien fracaso con pos2 >= len(word2)?
+    if pos1 >= len(word1):
+        if pos1 > len(word1):
+            return 
+        # lo unico que podría hacer es sacar de la pila e intentar a ver si armo la palabra
+        else: 
+            # intento ver si con lo que me queda en la pila logro armar la palabra
+            while not pila.isEmpty():
+                neword += pila.pop()
+            # si pude es caso exito
+            if neword == word2:
+                resp.append(pila.operaciones)
+                return 
+            # si no pude es caso fracaso
+            else: 
+                return 
     # caso base exito
     if neword == word2:
-        return resp.append(pila.operaciones)
+        resp.append(pila.operaciones)
+        return
+    
+    
+   
+    
+    # mientras el head sea distinto de la letra en la que voy de la segunda meto al stack
+    while pila.head != word2[pos2] and pos1 < len(word1):
+        pila.push(word1[pos1])
+        pos1 += 1
 
-    pila.push(word1[pos])
- 
     # caso recursivo
-    if pila.head == word2[pos]:
+    if pila.head == word2[pos2]:
+        respuesta = None
         # dos opciones por lo que hacemos copia de la pila
         pila1 = Stack()
         pila1.copy(pila)
 
+
         # pop inmediatamente 
         neword1 = neword + pila1.pop()
-        allposibleseqRec(word1, word2, pos +1, neword1, pila1, resp)
+        # si hago un pop avanzo en pos2
+        respuesta = allposibleseqRec(word1, word2, pos1, pos2 +1, neword1, pila1, resp)
+        if respuesta is not None:
+            resp.append(respuesta)
 
         # espero encontrarme otra más adelante
+        # por lo tanto hago push 
+        if pos1 < len(word1):
+            pila.push(word1[pos1])
+            respuesta = (allposibleseqRec(word1, word2, pos1 + 1, pos2, neword, pila, resp))
+            if respuesta is not None:
+                resp.append(respuesta)
 
-        allposibleseqRec(word1, word2, pos + 1, neword, pila, resp)
+        
+
+        
+        return 
+    else: 
+        ## caso de fracaso 
+        return 
+
+    
 
 
 def allposibleseq(word1, word2):
     pila = Stack()
-    return allposibleseqRec(word1, word2, 0, "",pila, [])
+    respuesta = []
+    allposibleseqRec(word1, word2, 0, 0, "",pila, respuesta)
+    
+    return respuesta
+
+print(allposibleseq("madam", "adamm"))
+print("\n")
+print(allposibleseq("bahama", "bahama"))
+print("\n")
+print(allposibleseq("eric", "rice"))
+print("\n")
+print(allposibleseq("aaaabbb", "ababaab"))
 
 
 
